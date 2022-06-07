dfb70c88d0aa0fe08ce712878f21aa049426402e
Laura Warnava <laurawwarnava@gmail.com>
2022-06-06 22:01:35 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRouts } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-    Teste
-    </div>
+    <BrowserRouter>
+      <AppRouts />
+    </BrowserRouter>
   );
 }
