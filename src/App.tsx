3bf62e3159a25a39adb500a64a068d0c09e3a494
Laura Warnava <laurawwarnava@gmail.com>
2022-06-07 23:43:16 -0300

    feat: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouts } from "./routes";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRouts />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRouts />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
